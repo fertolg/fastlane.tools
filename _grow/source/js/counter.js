583ca687bd5dd2adb706bc6dc7529de75158b552
nreddy216 <nidhireddy@google.com>
2017-12-13 15:30:42 -0800

    Add odometer, bring in odometer library

--- _grow/source/js/counter.js
+++ _grow/source/js/counter.js
@@ -1,3 +1,5 @@
+'use strict';
+
 (function(){
   var startedWithMinutes = null;
 
@@ -9,43 +11,26 @@
   var developerSecondsSavedPerSecond = 560;
   var timeUpdated = 1499885116;
 
-  var domTarget = document.querySelector('.hero__hours-counter__counter');
+  var odometerEl = document.getElementById('odometer');
 
-  function reloadThis() {
-    // get current time
-    // get difference in seconds from timeUpdated
-    // multiply difference in seconds by developerSecondsSavedPerSecond
-    // add to sum of developerSecondsSavedBefore and After
-
-    var currentEpoch = ((new Date()).getTime()) / 1000;
-
-    var developerSecondsSavedSinceUpdate = (currentEpoch - timeUpdated) * developerSecondsSavedPerSecond;
-    var seconds = developerSecondsSavedSinceUpdate + developerSecondsSavedBeforeAnalyticIngester + developerSecondsSavedAfterAnalyticIngester;
-
-    var minutes = seconds / 60; // to minutes
-    if (startedWithMinutes === null) {
-      startedWithMinutes = minutes;
-    }
-    var hours = minutes / 60; // to hours
-    var hoursAsArray = parseInt(hours).toString().split('');
-    var outputHtml = '';
-    var hoursOutput = [];
-    hoursAsArray.reverse();
-
-    for (var i = 0; i < hoursAsArray.length; i++) {
-      if ((i !== 0) && (i % 3 === 0)) {
-        hoursOutput[i] = '<span>'+ hoursAsArray[i] +'</span><span class="comma">,</span>';
-      }else{
-        hoursOutput[i] = '<span>'+ hoursAsArray[i] +'</span>';
-      }
-    }
-
-    hoursOutput.reverse();
-    for (var a = 0; a < hoursOutput.length; a++) {
-      outputHtml += hoursOutput[a] ;
-    }
-    domTarget.innerHTML = outputHtml;
+  var calculateHours = function() {
+      // get current time
+     // get difference in seconds from timeUpdated
+     // multiply difference in seconds by developerSecondsSavedPerSecond
+     // add to sum of developerSecondsSavedBefore and After
+     var currentEpoch = ((new Date()).getTime()) / 1000;
+     var developerSecondsSavedSinceUpdate = (currentEpoch - timeUpdated) * developerSecondsSavedPerSecond;
+     var seconds = developerSecondsSavedSinceUpdate + developerSecondsSavedBeforeAnalyticIngester + developerSecondsSavedAfterAnalyticIngester;
+     var minutes = seconds / 60; // to minutes
+     if (startedWithMinutes === null) {
+       startedWithMinutes = minutes;
+     }
+     var hours = parseInt(minutes / 60); // to hours
+     odometerEl.innerHTML = hours;
   }
-  reloadThis();
-  setInterval(reloadThis, 10000);
+
+  setTimeout(calculateHours, 50);
+  setInterval(function(){
+     calculateHours();
+  }, 5000);
 })();
--- _grow/source/js/hero.js
+++ _grow/source/js/hero.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import 'odometer';
+
 var hero = function() {
 
   /*
